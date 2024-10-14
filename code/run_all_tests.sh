73e02bdd87854fbf76fee2dc1feecb496eb2cb20
Shounak Sural <ssural@andrew.cmu.edu>
2024-10-14 03:58:50 -0400

    Created scripts for evaluating results

---
+++ code/run_all_tests.sh
@@ -0,0 +1,24 @@
+# python run_fshot_prompting.py --src_lang en
+# python run_fshot_prompting.py --src_lang hi
+python run_fshot_prompting.py --src_lang te
+
+python run_fshot_prompting.py --src_lang ar --dataset redfm
+python run_fshot_prompting.py --src_lang de --dataset redfm
+python run_fshot_prompting.py --src_lang en --dataset redfm
+python run_fshot_prompting.py --src_lang es --dataset redfm
+python run_fshot_prompting.py --src_lang fr --dataset redfm
+python run_fshot_prompting.py --src_lang it --dataset redfm
+python run_fshot_prompting.py --src_lang zh --dataset redfm
+
+
+python run_fshot_prompting.py --src_lang en --model_name gemma2
+python run_fshot_prompting.py --src_lang hi --model_name gemma2
+python run_fshot_prompting.py --src_lang te --model_name gemma2
+
+python run_fshot_prompting.py --src_lang ar --dataset redfm --model_name gemma2
+python run_fshot_prompting.py --src_lang de --dataset redfm --model_name gemma2
+python run_fshot_prompting.py --src_lang en --dataset redfm --model_name gemma2
+python run_fshot_prompting.py --src_lang es --dataset redfm --model_name gemma2
+python run_fshot_prompting.py --src_lang fr --dataset redfm --model_name gemma2
+python run_fshot_prompting.py --src_lang it --dataset redfm --model_name gemma2
+python run_fshot_prompting.py --src_lang zh --dataset redfm --model_name gemma2
