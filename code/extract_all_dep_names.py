242c765355173357c94bf7dbaf11b0f865a544fc
Shounak Sural <ssural@andrew.cmu.edu>
2024-10-23 21:21:23 -0400

    Dep parse list updated with better definitions

--- code/extract_all_dep_names.py
+++ code/extract_all_dep_names.py
@@ -1,3 +1,8 @@
+'''
+This file is used to generate a list of all the dependency parses used in both datasets.
+From the list, we use universaldependencies.org to find corresponding meanings.
+'''
+
 import json
 import os
 import glob
@@ -12,14 +17,25 @@ dependency_types = set()
 for json_file in glob.glob(os.path.join(root_dir, '**', '*.json'), recursive=True):
     with open(json_file) as f:
         json_data = json.load(f)
-
-    # Loop through each entry in the train data, if it exists
-    if 'train' in json_data:
-        for entry in json_data["train"]:
-            for dependency in entry["dep_graph"]:
-                # Add the dependency type (the second element in the list) to the set
-                dependency_types.add(dependency[1])
-
+    try:
+        # Loop through each entry in the train data, if it exists
+        if 'train' in json_data:
+            for entry in json_data["train"]:
+                for dependency in entry["dep_graph"]:
+                    # Add the dependency type (the second element in the list) to the set
+                    dependency_types.add(dependency[1])
+        if 'validation' in json_data:
+            for entry in json_data["train"]:
+                for dependency in entry["dep_graph"]:
+                    # Add the dependency type (the second element in the list) to the set
+                    dependency_types.add(dependency[1])
+        if 'test' in json_data:
+            for entry in json_data["train"]:
+                for dependency in entry["dep_graph"]:
+                    # Add the dependency type (the second element in the list) to the set
+                    dependency_types.add(dependency[1])
+    except:
+        pass
 # Convert the set to a sorted list
 unique_dependency_types = sorted(dependency_types)
 
